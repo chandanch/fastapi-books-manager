37ccee29c73509a04d3ab55edcdb08f29d7f30af
chandanch <chandan.cec107@gmail.com>
2023-09-28 21:01:44 +0530

    get all books API :bulb:

--- books.py
+++ books.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {"title": "My Dion Man", "author": "chandio", "category": "Deep Minds"},
+    {"title": "Findmens Gold", "author": "Fdio", "category": "Lean"},
+    {"title": "Visible Gold", "author": "Fdio", "category": "Lean"},
+    {"title": "Visible Sliver", "author": "chandio", "category": "Deep Minds"},
+]
+
 
 # first API
 @app.get("/healthcheck")
 async def health_check():
     return {"status": "OK", "message": "Service is up!"}
+
+
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
